1ff0c0a57809e4afb7f79a62173b82350283918f
Jeremy <jeremy.artero@gmail.com>
2019-12-12 16:08:55 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Connect to AlwaysOn SQL using NodeJS
+
+## Objectives
+Simple Nodejs example that open a connection using [node-odbc](https://www.npmjs.com/package/odbc) to a running [AlwaysOn SQL on DSE](https://docs.datastax.com/en/dse/6.7/dse-dev/datastax_enterprise/spark/alwaysOnSql.html)
+
+## Setup and running
+
+### Prerequisites
+
+
+  
---
+++ app.js
@@ -0,0 +1,37 @@
+const odbc = require('odbc');
+let util = require('util');
+let async = require('async');
+
+// building connection stream. Add Uid=username and Pwd=password if necessary.
+let CONNECTION_STRING = 'DRIVER=/opt/simba/spark/lib/64/libsparkodbc_sb64.so;HOST=localhost;PORT=10000';
+
+async function connectToDatabase() {
+    console.log("Connect to database")
+
+    const connection = await odbc.connect(CONNECTION_STRING).catch(console.log(err => console.error('There was an error trying to connect', err)));
+    const result = await connection.query("select * from brby_customers.core_data limit 10");
+
+    await connection.close();
+
+    // Another way to connect using object
+    // const connectionConfig = {
+    //     connectionString: 'DRIVER=/opt/simba/spark/lib/64/libsparkodbc_sb64.so;HOST=localhost;PORT=10000',
+    //     connectionTimeout: 10,
+    //     loginTimeout: 10
+    // }
+    // const connection2 = await odbc.connect(connectionConfig);
+
+    return result;
+}
+
+async function queryExample() {
+    const pool = await odbc.pool(CONNECTION_STRING);
+    const result = await pool.query('select * from brby_customers.core_data limit 10');
+    return result;
+}
+
+connectToDatabase()
+    .then((result) => console.log('Retrieved successfully data from cluster' + util.inspect(result)))
+    .catch(err => console.error('There was an error trying to connect', err));
+
+
