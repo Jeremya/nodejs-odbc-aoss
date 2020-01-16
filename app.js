40e53cb6f124fdfd0f30a1db197c4ec5188ceae0
Jeremy <jeremy.artero@gmail.com>
2020-01-16 11:25:48 +0100

    add a sql inner join request

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const odbc = require('odbc');
-let util = require('util');
-let async = require('async');
+const util = require('util');
+const async = require('async');
 
 // building connection stream. Add Uid=username and Pwd=password if necessary.
 let CONNECTION_STRING = 'DRIVER=/opt/simba/spark/lib/64/libsparkodbc_sb64.so;HOST=localhost;PORT=10000';
@@ -8,8 +8,9 @@ let CONNECTION_STRING = 'DRIVER=/opt/simba/spark/lib/64/libsparkodbc_sb64.so;HOS
 async function connectToDatabase() {
     console.log("Connect to database");
 
-    const connection = await odbc.connect(CONNECTION_STRING).catch(console.log(err => console.error('There was an error trying to connect', err)));
-    const result = await connection.query("select * from customers.data limit 10");
+    const connection = await odbc.connect(CONNECTION_STRING).catch(err => console.error('There was an error trying to connect', err));
+    //const result = await connection.query("select * from customers.core limit 10");
+    const result = await connection.query("select * from join_test.carts as a inner join join_test.users as b on a.user_id=b.user_id where a.user_id='6eb572ad-7cec-442b-9f50-c0da7938218c';");
 
     await connection.close();
 
