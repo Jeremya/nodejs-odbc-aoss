e1a74ddc30ef4310d5c767f58fb3007325863ef4
Jeremy <jeremy.artero@gmail.com>
2020-01-22 14:21:12 +0100

    update readme and connection string to add authentication mode

--- README.md
+++ README.md
@@ -24,3 +24,21 @@ Change connection string at the top of app.js using your own values.
 
 #### Run the app
 ```npm start```
+
+## Build Connection String
+Connection string should look like this ```DRIVER=/opt/simba/spark/lib/64/libsparkodbc_sb64.so;UID=test_spark;PWD=test_spark;HOST=127.0.0.1;PORT=9000;AuthMech=3;```
+
+
+### Authentication modes
+You can use different authentication mechanisms like Kerberos. See details on [Simba Linux Authentication page](https://www.simba.com/products/Hive/doc/ODBC_InstallGuide/linux/content/odbc/hi/authoptions.htm)
+Use AuthMech parameter to determine which authentication method you are using.
+
+| AuthMech value  | Authentication type  |
+|:---:|:---:|
+|0   | No Authentication  |
+| 1  | Using Kerberos  |
+| 2  | Using Username  | 
+| 3  | Using Username and Password  | 
+| 6  | Using Windows Azure HDInsight Service  | 
+
+Use ```UID``` and ```PWD``` parameters to put username and password.
--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const odbc = require('odbc');
 const util = require('util');
-const async = require('async');
 
 // building connection stream. Add Uid=username and Pwd=password if necessary.
-let CONNECTION_STRING = 'DRIVER=/opt/simba/spark/lib/64/libsparkodbc_sb64.so;HOST=localhost;PORT=10000';
+let CONNECTION_STRING = 'DRIVER=/opt/simba/spark/lib/64/libsparkodbc_sb64.so;UID=user;PWD=password;HOST=127.0.0.1;PORT=9000;AuthMech=3;';
+
 
 async function connectToDatabase() {
-    console.log("Connect to database");
-
     const connection = await odbc.connect(CONNECTION_STRING).catch(err => console.error('There was an error trying to connect', err));
-    //const result = await connection.query("select * from customers.core limit 10");
     const result = await connection.query("select * from join_test.carts as a inner join join_test.users as b on a.user_id=b.user_id where a.user_id='6eb572ad-7cec-442b-9f50-c0da7938218c';");
 
     await connection.close();
